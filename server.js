ea1956aa36abd8b6cdb79676e8d33fabff12f79c
victordrumond <victordrumond@hotmail.com>
2022-01-06 21:14:52 -0300

    Update code with POST requests to upload file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Require dependencies & basic configuration
 const express = require('express');
 const cors = require('cors');
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +14,15 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// POST request: upload file
+app.post('/api/file/analyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  });
 });
 
 // Listen connection on port
